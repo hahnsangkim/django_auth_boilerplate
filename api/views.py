c57593ef4ae3ff37ccbf631a168d5bfa643222c4
Hahnsang Kim <hkim@inspro9.com>
2021-07-31 13:27:02 -0700

    feature: api

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getRoutes(request):
+
+    routes = [
+        {'GET': '/api/projects'},
+        {'GET': '/api/projects/id'},
+        {'POST': '/api/projects/id/vote'},
+
+        {'POST': '/api/users/token'},
+        {'POST': '/api/users/token/refresh'},
+    ]
+    return Response(routes)
